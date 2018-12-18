7ad7589ca0f3f1b1363de6a37d91792042c76ab9
Jay Goss <jayspgoss@gmail.com>
2018-12-18 16:12:54 -0500

    starting

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,3 @@
 -- write insert queries to populate the burgers table with at least three entries.
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES (1, "Whopper", FALSE), (2, "Big Mac", FALSE), (3, "Slider", FALSE);
