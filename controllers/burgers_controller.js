43c218434e9898863cbdc7f78524f3bdaa0e656b
Jay Goss <jayspgoss@gmail.com>
2018-12-20 16:27:47 -0500

    broken updates

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,8 +2,16 @@
 // burger.js
 var express = require('express');
 var router = express.Router();
+var burger = require("models/burger.js");
 
+// get
+router.get("/burgers", function(req,res) {
 
+// post
+router.post
+
+// put
+router.put
 
 // Create the router for the app, and export the router at the end of your file.
 module.exports = router;
